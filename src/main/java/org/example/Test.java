e6f7e7dfee87ae91e8f166a01077dda4af53d229
dustjq1004 <dustjq1005@naver.com>
2023-12-23 22:29:30 +0900

    PR Template Test

--- src/main/java/org/example/Test.java
+++ src/main/java/org/example/Test.java
@@ -3,7 +3,7 @@ package org.example;
 public class Test {
 
     public Test() {
-        System.out.println("테스트 커밋");
+        System.out.println("테스트 커밋1");
         System.out.println("테스트 커밋2");
     }
 }
